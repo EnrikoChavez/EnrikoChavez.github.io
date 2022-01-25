09d9cd3804539b88b65c3277b1b0967cb99ecb94
Enriko Chavez <enriko@mit.edu>
2022-01-25 04:38:01 -0500

    many CSS changes and textbox changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,15 @@ import './About.css'
 
 function About() {
     return (
-        <div>
-            About me
+        <div className="home">
+            <div className="padding" id="top"/>
+            <div className="about-blob">
+                I am an Electrical Engineering and Computer Science gradute from MIT. Originally a physics major, I've enjoyed spending time learning about the world around me in detail, and how it came to be. My goal being to contribute in a creative and informed way in anything I do. Currently I spend my days developing on distributed systems.
+            </div>
+            <div className="about-blob">
+                This website is reactive!
+            </div>   
+            <div className="padding" id="bottom"/>
         </div>
     )
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
             <div className="home-bar">
                 <img className="profile-picture" src="/images/Enriko.jpg" alt=""/>
                 <div className="info-box">
-                    <div className="stuff">hello</div>
+                    <div className="stuff">hello! this is my personal sandbox website. I'll be using this site to explore my techy curiosity</div>
                 </div>
             </div>
             <div className="padding" id="bottom"/>
