7229c41153da2c27b5c6e4c282bed4b948c0f478
Enriko Chavez <enriko@mit.edu>
2022-01-25 18:59:54 -0500

    test commit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,7 @@ function About() {
                 I am an Electrical Engineering and Computer Science gradute from MIT. Originally a physics major, I've enjoyed spending time learning about the world around me in detail, and how it came to be. My goal being to contribute in a creative and informed way in anything I do. Currently I spend my days developing on distributed systems.
             </div>
             <div className="about-blob">
-                This website is reactive!
+                This website is reactive! 
             </div>   
             <div className="padding" id="bottom"/>
         </div>
