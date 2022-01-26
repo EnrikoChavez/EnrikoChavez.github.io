cfb8d4a2cd7f67c0bc4f42b704c095196e686901
Enriko Chavez <enriko@mit.edu>
2022-01-26 00:18:17 -0500

    change wordings of about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ function About() {
         <div className="home">
             <div className="padding" id="top"/>
             <div className="about-blob">
-                I am an Electrical Engineering and Computer Science graduate from MIT. Originally a physics major, I've enjoyed spending time learning about the world around me in detail, and how it came to be. My goal is to contribute in a creative and informed way in anything I do.
+                I am an Electrical Engineering and Computer Science graduate from MIT. Originally a physics major, I enjoy spending time learning about the world around me in detail, how it came to be, and exploring ways on how to better it. My goal is to contribute in a creative and informed way in anything I do.
             </div>
             <div className="about-blob">
                 This website is reactive! 
